fd23a3cf3813c0f0a7e62d05bb0c57d20ea868f0
David <davidpjoseph11@gmail.com>
2021-06-04 15:23:33 +0530

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
+import Header from "./Header"
 
 
 function App() {
   return (
     <div>
-      Phone Directory
+      <Header/>
+      <button>Add</button>
+      <div>
+        <span>Name =</span> <br />
+        <span>Phone=</span>
+      </div>
     </div>
+
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header =function(){
+    return(
+        <div>
+        Phone Directory
+        </div>
+    )
+}
+
+
+
+export default Header;
